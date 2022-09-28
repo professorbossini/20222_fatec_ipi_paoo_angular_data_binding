701f41a1ec5ad7f58731d8db7fec44ba63bf8331
Rodrigo Bossini <professorbossini@gmail.com>
2022-09-28 17:57:55 -0300

    feat(apostila04): listagem 2.4.24 concluida, ainda sem two way data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   nome;
+  esconderCaixa = true
+  numero: number
+  escolher(): void{
+    this.numero = Math.floor(Math.random() * 100) + 1
+  }
 
   alterarNome(nome){
     console.log(nome.target.value)
@@ -17,8 +22,9 @@ export class AppComponent {
     return Math.floor(Math.random() * 6) + 1
   }
   
-  adicionar(){
-    console.log("Adicionando...")
+  adicionar(nomeInput){
+    this.nome = nomeInput.value
+    this.esconderCaixa = nomeInput.value.length <= 0
   }
   // idade = 20
 }
